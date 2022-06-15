120d41235330aa6b1b8a4fcdd51d0e6b71db986d
Nabiel Alif <nabiel.alif01@gmail.com>
2022-06-16 04:05:09 +0700

    Toggle form when map clicked

--- script.js
+++ script.js
@@ -11,39 +11,61 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
-    position => {
-      const latitude = position.coords.latitude;
+    function (position) {
+      const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.com/maps/@-${latitude},${longitude}`);
       const coords = [latitude, longitude];
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      map.on('click', e => {
-        const lat = e.latlng.lat;
-        const lng = e.latlng.lng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 200,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('yea man')
-          .openPopup();
+      map.on('click', function (mapE) {
+        mapEvent = mapE; //assigning the event to global variable
+        form.classList.remove('hidden');
+        inputDistance.focus();
       });
     },
 
     () => alert('cannot detect your location')
   );
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  const lat = mapEvent.latlng.lat;
+  const lng = mapEvent.latlng.lng;
+
+  //clear input fields
+  inputCadence.value =
+    inputDistance.value =
+    inputElevation.value =
+    inputDuration.value =
+      '';
+
+  // display marker
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 200,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('yea man')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
