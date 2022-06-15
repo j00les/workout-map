31a7d35c2bad06f5e44255aad4fa98c0cf37865e
Nabiel Alif <nabiel.alif01@gmail.com>
2022-06-15 16:50:22 +0700

    Add leaflet map and customize popup

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      const latitude = position.coords.latitude;
+      const { longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@-${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', e => {
+        const lat = e.latlng.lat;
+        const lng = e.latlng.lng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 200,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('yea man')
+          .openPopup();
+      });
+    },
+
+    () => alert('cannot detect your location')
+  );
